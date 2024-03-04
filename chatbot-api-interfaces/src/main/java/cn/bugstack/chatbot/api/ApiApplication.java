1942e2e5545c6582715a15d552243ffeceba9f68
junhan-zhao <junhan_zhao@163.com>
2024-03-04 14:43:58 +0800

    init add project

---
+++ chatbot-api-interfaces/src/main/java/cn/bugstack/chatbot/api/ApiApplication.java
@@ -0,0 +1,21 @@
+package cn.bugstack.chatbot.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @projectName: chatbot-api
+ * @package: cn.bugstack.chatbot.api
+ * @className: ApiApplication
+ * @author: junhan Zhao
+ * @description:  启动入口
+ * @date: 2024/3/4 14:04
+ * @version: 1.0
+ */
+@SpringBootApplication
+public class ApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+
+}
---
+++ chatbot-api-interfaces/src/test/java/cn/bugstack/chatbot/api/test/ApiTest.java
@@ -0,0 +1,14 @@
+package cn.bugstack.chatbot.api.test;
+
+/**
+ * @projectName: chatbot-api
+ * @package: cn.bugstack.chatbot.api.test
+ * @className: ApiTest
+ * @author: junhan Zhao
+ * @description:  单元测试
+ * @date: 2024/3/4 14:30
+ * @version: 1.0
+ */
+
+public class ApiTest {
+}
