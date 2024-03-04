ecf74dca7050a7437bf4193c15ba08539756ba57
junhan-zhao <junhan_zhao@163.com>
2024-03-04 16:28:50 +0800

    add doc and branch

--- chatbot-api-interfaces/src/test/java/cn/bugstack/chatbot/api/test/ApiTest.java
+++ chatbot-api-interfaces/src/test/java/cn/bugstack/chatbot/api/test/ApiTest.java
@@ -1,5 +1,15 @@
 package cn.bugstack.chatbot.api.test;
 
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.util.EntityUtils;
+import org.apache.tomcat.util.http.fileupload.MultipartStream;
+
+import java.io.IOException;
+
 /**
  * @projectName: chatbot-api
  * @package: cn.bugstack.chatbot.api.test
@@ -11,4 +21,20 @@ package cn.bugstack.chatbot.api.test;
  */
 
 public class ApiTest {
+
+    public void query_unansered_questions() throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+        HttpGet get = new HttpGet(" ");
+        get.addHeader("cookie", " ");
+        get.addHeader("Content-Type", " ");
+
+        CloseableHttpResponse response = httpClient.execute(get);
+        if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK){
+            String res = EntityUtils.toString(response.getEntity());
+            System.out.println(res);
+        }else{
+            System.out.println(response.getStatusLine().getStatusCode());
+        }
+
+    }
 }
